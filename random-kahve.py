16d7029c58631c8753178fa42504482bf4b5727d
yasinguzel <>
2018-04-20 21:53:24 +0300

    initial commit

---
+++ random-kahve.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+isimler = ["Oğuzhan 🤬 ","Yasin 😎 "]
+taraf = ["sağ", "sol"]
+
+print str(isimler[random.getrandbits(1)]) + " " + str(taraf[random.getrandbits(1)]) + "daki kahveyi içer. ☕️"
+
